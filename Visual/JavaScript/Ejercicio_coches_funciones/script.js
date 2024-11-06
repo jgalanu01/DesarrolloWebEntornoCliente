8d729ea0e52678e005fb2fc776fb0ee0e3dbf835
Jesús <145713220+jgalanu01@users.noreply.github.com>
2024-11-06 19:52:47 +0100

    ejercicio ordenar coches

--- Visual/JavaScript/Ejercicio_coches_funciones/script.js
+++ Visual/JavaScript/Ejercicio_coches_funciones/script.js
@@ -1,79 +1,73 @@
-
 document.addEventListener('DOMContentLoaded', inicio);
-const cajaPrincipal = document.querySelector(".container")
+
+const cajaPrincipal = document.querySelector(".container");
 const btnKms = document.querySelector("#ord_kms");
 const btnMarca = document.querySelector("#ord_marca");
 const btnConsumo = document.querySelector("#ord_consumo");
-const btnkms2 = document.querySelector("#ord_kms2");
-//construyo el array que va a contener el fichero json
+
 const coches = [];
+let ordenAscendente = true;
+
 function inicio() {
-    //volcado del json a data
     fetch('coches.json')
         .then(response => response.json())
         .then(data => cargar(data));
-
 }
-function cargar(data) {
-    //pasar de data a un array
-    const cochesContainer = document.querySelector('.container');
 
-    //cargar desde el json hasta un array
+function cargar(data) {
     data.forEach(coche => {
         coches.push(coche);
     });
-    //filtrar
-    btnKms.onclick = ordenarPorKms;
-    btnMarca.onclick = ordenarPorMarca;
-    btnConsumo.onclick = ordenarPorConsumo;
-    btnkms2.onclick = ordenarPorKms2;
-    //btnMarca2.onclick=ordenarPorMarca2;
-    //btnConsumo2.onclick=ordenarPorConsumo2;
+    btnKms.addEventListener('click', () => ordenarYMostrar("kms"));
+    btnMarca.addEventListener('click', () => ordenarYMostrar("marca"));
+    btnConsumo.addEventListener('click', () => ordenarYMostrar("consumo"));
+}
 
-}
-function ordenarPorKms() {
-    const listadoOrdenadoKms = coches.slice().sort((a, b) => a.kms - b.kms);
-    mostrarListadoOrdenado(listadoOrdenadoKms);
-}
-function ordenarPorConsumo() {
-    const listadoOrdenadoConsumo = coches.slice().sort((a, b) => a.consumo - b.consumo)
-    mostrarListadoOrdenado(listadoOrdenadoConsumo);
+function ordenarYMostrar(criterio) {
+    let listaOrdenar;
+    
+    if (criterio === "kms") {
+        listaOrdenar = coches.slice().sort((a, b) => a.kms - b.kms);
+    } else if (criterio === "marca") {
+        listaOrdenar = coches.slice().sort((a, b) => a.marca.localeCompare(b.marca));
+    } else if (criterio === "consumo") {
+        listaOrdenar = coches.slice().sort((a, b) => a.consumo - b.consumo);
+    }
+    // Al cambiar la variable, le hacemos el reverse
+    if (!ordenAscendente) {
+        listaOrdenar.reverse();
+    }
+    
+    //La variable cambia entre true o false cada vez que se pulse el botón 
+    ordenAscendente = !ordenAscendente;
 
+    mostrarListadoOrdenado(listaOrdenar);
 }
-function ordenarPorMarca() {
-    const listadoOrdenaPorMarca = coches.slice().sort((a, b) => a.marca.localeCompare(b.marca));
-    mostrarListadoOrdenado(listadoOrdenaPorMarca);
-}
-function ordenarPorKms2() {
-    const listadoOrdenadoKms = coches.slice().sort((a, b) => a.kms - b.kms).reverse();
-    mostrarListadoOrdenado(listadoOrdenadoKms);
-}
-//mostrar la lista lista ordenada
+
 function mostrarListadoOrdenado(coches) {
     limpiarCaja();
-    if (coches.length > 0) {
-        coches.forEach(element => {
-            const spKms = document.createElement("small");
-            spKms.textContent = "kms:" + element.kms;
-            const spMarca = document.createElement("small");
-            spMarca.textContent = "  MARCA: " + element.marca;
-            const spConsumo = document.createElement("small");
-            spConsumo.textContent = "  CONSUMO: " + element.consumo;
-            const spPrecio = document.createElement("small");
-            spPrecio.textContent = "  PRECIO:" + element.precio;
-            const salto = document.createElement("br");
-            cajaPrincipal.appendChild(spKms)
-            cajaPrincipal.appendChild(spMarca)
-            cajaPrincipal.appendChild(spConsumo)
-            cajaPrincipal.appendChild(spPrecio)
-            cajaPrincipal.appendChild(salto)
-
-        })
-    }
+    coches.forEach(element => {
+        const spKms = document.createElement("small");
+        spKms.textContent = "kms: " + element.kms;
+        const spMarca = document.createElement("small");
+        spMarca.textContent = "  MARCA: " + element.marca;
+        const spConsumo = document.createElement("small");
+        spConsumo.textContent = "  CONSUMO: " + element.consumo;
+        const spPrecio = document.createElement("small");
+        spPrecio.textContent = "  PRECIO: " + element.precio;
+        const salto = document.createElement("br");
+        cajaPrincipal.appendChild(spKms);
+        cajaPrincipal.appendChild(spMarca);
+        cajaPrincipal.appendChild(spConsumo);
+        cajaPrincipal.appendChild(spPrecio);
+        cajaPrincipal.appendChild(salto);
+    });
 }
+
 function limpiarCaja() {
-    while (cajaPrincipal.firstElementChild) {
-        cajaPrincipal.removeChild(cajaPrincipal.firstElementChild);
-    }
+    Array.from(cajaPrincipal.children).forEach(child => {
+        if (!child.classList.contains('btn')) {
+            cajaPrincipal.removeChild(child);
+        }
+    });
 }
-
