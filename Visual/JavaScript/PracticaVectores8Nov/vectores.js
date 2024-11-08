3a0d7f7c27b542db989c071adc7398e5654740de
Jesús Galán <jgalanu01@educarex.es>
2024-11-08 11:52:29 +0100

    nuevo

---
+++ Visual/JavaScript/PracticaVectores8Nov/vectores.js
@@ -0,0 +1,23 @@
+window.addEventListener("DOMContentLoaded", inicio);
+
+function inicio() {
+  const vector1 = [2, 5, 2, 4, 6, 8, 9, 6];
+  const vector2 = ["paco", "lola", "pedro"];
+  const vector3 = [
+    {
+      titulo: 'Los lunes al sol',
+      minutos: 120
+    },
+    {
+      titulo: 'La delgada línea roja',
+      minutos: 120
+    },
+    {
+      titulo: "Bienvenido MrMarsall",
+      minutos: 93
+    }
+  ]
+
+
+
+}
