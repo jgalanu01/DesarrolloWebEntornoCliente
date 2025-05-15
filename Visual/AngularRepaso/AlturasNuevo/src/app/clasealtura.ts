9ec9592eac539237c6a8319b6e2404d9100d0508
Jesus <jesusgalanutrera@gmail.com>
2025-05-15 23:22:14 +0200

    mare meua

---
+++ Visual/AngularRepaso/AlturasNuevo/src/app/clasealtura.ts
@@ -0,0 +1,7 @@
+export class Clasealtura {
+  Provincia!:string;
+  SituacionAltMax!:string;
+  AlturaMaxima!:number;
+  SituacionAltMin!:string;
+  AlturaMinima!:number;
+}
---
+++ Visual/AngularRepaso/AlturasNuevo/src/app/listado-alturas/listado-alturas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-alturas',
+  templateUrl: './listado-alturas.component.html',
+  styleUrls: ['./listado-alturas.component.css']
+})
+export class ListadoAlturasComponent {
+
+}
